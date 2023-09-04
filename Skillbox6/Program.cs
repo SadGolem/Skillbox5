9fa05c1f08cb9d6ea967e0b3d079fa1316921505
Марина Зубцова <87470059+SadGolem@users.noreply.github.com>
2023-09-04 22:58:58 +0700

    Добавьте файлы проекта.

---
+++ Skillbox6/Program.cs
@@ -0,0 +1,44 @@
+﻿
+/*static string[] SplitText(string text)
+{
+    string[] mas = text.Split();
+    return OutputText(mas);
+}
+*/
+static string[] OutputText(string[] text)
+{
+    for (int i = 0; i < text.Length; i++)
+    {
+        string output = text[i];
+        Console.WriteLine(output);
+    }
+    return text;
+}
+
+
+/*Console.WriteLine("Напишите предложение:");
+SplitText(Console.ReadLine());*/
+
+/*-----------------------------------Task2------------------------------------ -*/
+
+Console.WriteLine("Напишите предложение:");
+ReversWords(Console.ReadLine());
+
+
+static string[] SplitText(string text)
+{
+    string[] mas = text.Split();
+    return mas;
+}
+
+static string[] ReversWords(string inputPhrase)
+{
+    string[] mas = SplitText(inputPhrase);
+    string[] masReverse = new string[mas.Length];
+    for (int i = 0; i < mas.Length; i++)
+    {
+        masReverse[i] += mas[mas.Length - 1 - i];
+    }
+    return OutputText(masReverse);
+}
+
